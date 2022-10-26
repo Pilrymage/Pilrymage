09b011cfeb997729965262380dd381be2d016cf5
Pilrymage <156th@pilrymage.xyz>
2022-10-26 20:04:14 +0800

    first upload

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+import { defineUserConfig } from 'vuepress'
+export default defineUserConfig({
+    lang: 'en-US',
+    title: 'Pilrymage\'s Haven',
+    description: 'Just playing around',
+    head: [['link']]
+  })
